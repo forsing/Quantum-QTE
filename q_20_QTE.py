ef73ea6b64e7b1078919d372b60e268b0415b83b
ForsIng <forsing@gmail.com>
2026-03-20 17:20:17 +0100

    Update q_20_QTE.py

--- q_20_QTE.py
+++ q_20_QTE.py
@@ -17,8 +17,8 @@ np.random.seed(SEED)
 random.seed(SEED)
 algorithm_globals.random_seed = SEED
 
-CSV_DRAWN = "/Users/4c/Desktop/GHQ/data/loto7hh_4582_k22.csv"
-CSV_ALL   = "/Users/4c/Desktop/GHQ/data/kombinacijeH_39C7.csv"
+CSV_DRAWN = "/data/loto7hh_4582_k22.csv"
+CSV_ALL   = "/data/kombinacijeH_39C7.csv"
 
 MIN_VAL = [1, 2, 3, 4, 5, 6, 7]
 MAX_VAL = [33, 34, 35, 36, 37, 38, 39]
@@ -212,7 +212,7 @@ Graf svih kombinacija: /Users/4c/Desktop/GHQ/data/kombinacijeH_39C7.csv
 
 ==================================================
 Predikcija (QTE, deterministicki, seed=39):
-[1, 8, 11, 14, 23, 35, 38]
+[1, 8, x, y, z, 35, 38]
 ==================================================
 """
 
